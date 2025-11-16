4f51331322a7571340788374bbef249c6d32ef9a
umbrashia <shantanu34@outlook.com>
2025-11-17 01:37:54 +0530

    spped up

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JavaObject = exports.Gateway = void 0;
-var gateway_1 = require("./gateway");
-Object.defineProperty(exports, "Gateway", { enumerable: true, get: function () { return gateway_1.Gateway; } });
-var java_object_1 = require("./java-object");
-Object.defineProperty(exports, "JavaObject", { enumerable: true, get: function () { return java_object_1.JavaObject; } });
